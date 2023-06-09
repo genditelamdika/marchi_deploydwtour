6cc0695c107410eb364aeb12c46f4e493f9dd2d9
genditelamdika <gendydika@gmail.com>
2023-06-10 03:07:56 +0700

    comment handler

--- server/dto/transaction/transaction_req.go
+++ server/dto/transaction/transaction_req.go
@@ -3,14 +3,13 @@ package transactiondto
 import "mytask/models"
 
 type CreateTransactionRequest struct {
-	Counterqty int    `json:"counterqty" `
-	Total      int    `json:"total" `
-	Status     string `json:"status"  gorm:"type: varchar(255)"`
+	Counterqty int    `json:"counterqty" form:"counterqty"`
+	Total      int    `json:"total" form:"total"`
+	Status     string `json:"status" form:"status" gorm:"type: varchar(255)"`
 	// Attachment string      `json:"attachment" form:"attachment" gorm:"type: varchar(255)"`
 	TripID int         `json:"tripid" form:"tripid"`
 	Trip   models.Trip `json:"trip" `
 	UserID int         `json:"userid" form:"userid"`
-	User   models.User `json:"user"`
 }
 
 type UpdateTransactionRequest struct {
@@ -21,6 +20,5 @@ type UpdateTransactionRequest struct {
 	TripID int         `json:"tripid" form:"tripid"`
 	Trip   models.Trip `json:"trip" form:"trip" `
 	UserID int         `json:"userid" form:"userid"`
-	User   models.User `json:"user"`
 	// Category    models.Category `json:"category" form:"category" validate:"required"
 }
