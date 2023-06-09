c31152e14accfcb7f94bc89fe06c7004b3274a58
genditelamdika <gendydika@gmail.com>
2023-06-09 23:14:31 +0700

    without cloudinary

--- server/pkg/middleware/uploadFile.go
+++ server/pkg/middleware/uploadFile.go
@@ -1,9 +1,13 @@
 package middleware
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
+	dto "mytask/dto/result"
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -17,21 +21,21 @@ func UploadFile(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 
 		// handle extensi filename
-		// ext := filepath.Ext(file.Filename)
-		// allowedExts := []string{".png", ".jpg", ".jpeg"} // Ekstensi yang diperbolehkan
+		ext := filepath.Ext(file.Filename)
+		allowedExts := []string{".png", ".jpg", ".jpeg"} // Ekstensi yang diperbolehkan
 
-		// // check valid extensi
-		// validExt := false
-		// for _, allowedExt := range allowedExts {
-		// 	if strings.ToLower(ext) == allowedExt {
-		// 		validExt = true
-		// 		break
-		// 	}
-		// }
+		// check valid extensi
+		validExt := false
+		for _, allowedExt := range allowedExts {
+			if strings.ToLower(ext) == allowedExt {
+				validExt = true
+				break
+			}
+		}
 
-		// if !validExt {
-		// 	return c.JSON(http.StatusBadRequest, dto.ErrorResult{Code: http.StatusBadRequest, Message: "Invalid file extension"})
-		// }
+		if !validExt {
+			return c.JSON(http.StatusBadRequest, dto.ErrorResult{Code: http.StatusBadRequest, Message: "Invalid file extension"})
+		}
 		//open file
 		src, err := file.Open()
 		if err != nil {
@@ -39,8 +43,8 @@ func UploadFile(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 		defer src.Close()
 		//Membuat file sementara dengan nama unik di direktori "uploads" menggunakan ioutil.TempFile(). Nama file akan dimulai dengan "image-" dan diikuti dengan karakter acak.
-		tempFile, err := ioutil.TempFile("uploads", "image-*.png")
-		// +ext
+		tempFile, err := ioutil.TempFile("uploads", "image-*"+ext)
+
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, err)
 		}
@@ -52,8 +56,8 @@ func UploadFile(next echo.HandlerFunc) echo.HandlerFunc {
 
 		data := tempFile.Name()
 
-		// update := strings.Split(data, "\\")[1]
-		// fmt.Println(update)
+		update := strings.Split(data, "\\")[1]
+		fmt.Println(update)
 
 		c.Set("dataFile", data)
 		return next(c)
