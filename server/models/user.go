9b21b8cefdec018b1fe7c454df551aee12c486f0
genditelamdika <gendydika@gmail.com>
2023-06-10 03:04:15 +0700

    comment handler cek tran

--- server/models/user.go
+++ server/models/user.go
@@ -1,16 +1,16 @@
 package models
 
 type User struct {
-	ID          int           `json:"id"`
-	Name        string        `json:"fullname" gorm:"type: varchar(255)"`
-	Email       string        `json:"email" gorm:"type: varchar(255)"`
-	Password    string        `json:"password" gorm:"type: varchar(255)"`
-	Phone       string        `json:"phone" gorm:"type: varchar(255)"`
-	Address     string        `json:"address" gorm:"type: varchar(255)"`
-	Role        string        `json:"role" grom:"type:varchar(255)"`
-	Gender      string        `json:"gender" grom:"type:varchar(255)"`
-	Transaction []Transaction `json:"transaction"`
-	Image       string        `json:"image" form:"image" grom:"type: varchar(255)"`
+	ID       int    `json:"id"`
+	Name     string `json:"fullname" gorm:"type: varchar(255)"`
+	Email    string `json:"email" gorm:"type: varchar(255)"`
+	Password string `json:"password" gorm:"type: varchar(255)"`
+	Phone    string `json:"phone" gorm:"type: varchar(255)"`
+	Address  string `json:"address" gorm:"type: varchar(255)"`
+	Role     string `json:"role" grom:"type:varchar(255)"`
+	Gender   string `json:"gender" grom:"type:varchar(255)"`
+	// Transaction []Transaction `json:"transaction"`
+	Image string `json:"image" form:"image" grom:"type: varchar(255)"`
 }
 
 func (User) TableName() string {
