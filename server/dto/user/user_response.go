2b807d53c02d0c2b1b7b27815faf86be9c1862af
genditelamdika <gendydika@gmail.com>
2023-06-09 20:59:55 +0700

    move user 3

---
+++ server/dto/user/user_response.go
@@ -0,0 +1,11 @@
+package usersdto
+
+type UserResponse struct {
+	ID       int    `json:"id"`
+	Name     string `json:"fullname" `
+	Email    string `json:"email" `
+	Password string `json:"password" `
+	Phone    string `json:"phone" `
+	Address  string `json:"address" `
+	Role     string `json:"role" `
+}
