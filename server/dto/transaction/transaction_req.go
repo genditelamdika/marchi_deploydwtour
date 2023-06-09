3822305f9d30a9c5047342e070bc2f5727a0c800
genditelamdika <gendydika@gmail.com>
2023-06-10 04:21:02 +0700

    ADD preload user at transaction n create profilerespon

--- server/dto/transaction/transaction_req.go
+++ server/dto/transaction/transaction_req.go
@@ -7,10 +7,10 @@ type CreateTransactionRequest struct {
 	Total      int    `json:"total" form:"total"`
 	Status     string `json:"status" form:"status" gorm:"type: varchar(255)"`
 	// Attachment string      `json:"attachment" form:"attachment" gorm:"type: varchar(255)"`
-	TripID int         `json:"tripid" form:"tripid"`
-	Trip   models.Trip `json:"trip" `
-	UserID int         `json:"userid" form:"userid"`
-	User   models.User `json:"user"`
+	TripID int                        `json:"tripid" form:"tripid"`
+	Trip   models.Trip                `json:"trip" `
+	UserID int                        `json:"userid" form:"userid"`
+	User   models.UserProfileResponse `json:"user"`
 }
 
 type UpdateTransactionRequest struct {
