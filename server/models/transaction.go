3256b155b007062fd1849728e07bb61bcb876cc9
genditelamdika <gendydika@gmail.com>
2023-06-09 21:52:55 +0700

    delete primary key

--- server/models/transaction.go
+++ server/models/transaction.go
@@ -4,10 +4,9 @@ type Transaction struct {
 	ID         int    `json:"id"`
 	Counterqty int    `json:"counterqty" form:"counterqty"`
 	Total      int    `json:"total" form:"total"`
-	Status     string `json:"status" form:"status" gorm:"type: varchar(255)"`
-
-	TripID int  `json:"tripid" form:"tripid" `
-	Trip   Trip `json:"trip" `
-	UserID int  `json:"userid"`
-	User   User `json:"user"`
+	Status     string `json:"status" form:"status" gorm:"type:varchar(255)"`
+	TripID     int    `json:"tripid" form:"tripid" `
+	Trip       Trip   `json:"trip" `
+	UserID     int    `json:"userid"`
+	User       User   `json:"user"`
 }
--- server/models/trip.go
+++ server/models/trip.go
@@ -1,17 +1,17 @@
 package models
 
 type Trip struct {
-	ID             int    `json:"id" gorm:"primary_key:auto_increment"`
-	Title          string `json:"title" gorm:"type: varchar(255)"`
-	Country        string `json:"country" gorm:"type: varchar(255)"`
-	Accomodation   string `json:"accomodation" gorm:"type: varchar(255)"`
-	Transportation string `json:"transportation" gorm:"type: varchar(255)"`
-	Eat            string `json:"eat" gorm:"type: varchar(255)"`
-	Day            int    `json:"day" gorm:"type: int"`
-	Night          int    `json:"night" gorm:"type: int"`
+	ID             int    `json:"id"`
+	Title          string `json:"title" gorm:"type:varchar(255)"`
+	Country        string `json:"country" gorm:"type:varchar(255)"`
+	Accomodation   string `json:"accomodation" gorm:"type:varchar(255)"`
+	Transportation string `json:"transportation" gorm:"type:varchar(255)"`
+	Eat            string `json:"eat" gorm:"type:varchar(255)"`
+	Day            int    `json:"day" gorm:"type:int"`
+	Night          int    `json:"night" gorm:"type:int"`
 	DateTrip       string `json:"datetrip" form:"datetrip"`
-	Price          int    `json:"price" gorm:"type: int(99)"`
-	Quota          int    `json:"quota" gorm:"type: int"`
-	Description    string `json:"description" form:"description" gorm:"type: varchar(255)"`
-	Image          string `json:"image" form:"image" grom:"type: varchar(255)"`
+	Price          int    `json:"price" gorm:"type:int"`
+	Quota          int    `json:"quota" gorm:"type:int"`
+	Description    string `json:"description" form:"description" gorm:"type:varchar(255)"`
+	Image          string `json:"image" form:"image" grom:"type:varchar(255)"`
 }
